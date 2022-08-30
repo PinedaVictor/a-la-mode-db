35e2b28f34338985840f138e79c6355c311efa6b
PinedaVictor <pinedavictor095@gmail.com>
2022-08-29 21:14:35 -0700

    Added nodemon dev script and tsc build script

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({ sup: true });
+});
+
+app.listen(port, () => {
+  console.log(`API listening on port http://localhost:${port}/`);
+});
