afc51e1139b0564559f620b6f36c970884010623
PinedaVictor <pinedavictor095@gmail.com>
2022-10-02 16:26:16 -0700

    Removed functions

--- functions/src/zone/parse.ts
+++
@@ -1,8 +0,0 @@
-import { userModel } from '../../../src/models/user';
-
-export const gettingAllUsers = async () => {
-  console.log('Getting all Users');
-  const users = await userModel.getAllUser();
-  console.log('THe users are:', users);
-  return users;
-};
